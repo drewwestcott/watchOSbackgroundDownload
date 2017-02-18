ae2616902f34baaf6c4127b3d9acc9609bf6d5d9
Drew Westcott <code@drewwestcott.co.uk>
2017-02-18 23:23:11 +0000

    Trying to get a file to download in the background - never completes

--- downloadFile WatchKit Extension/InterfaceController.swift
+++ downloadFile WatchKit Extension/InterfaceController.swift
@@ -7,25 +7,144 @@
 //
 
 import WatchKit
-import Foundation
 
+class InterfaceController: WKInterfaceController, WKExtensionDelegate, URLSessionDownloadDelegate {
+	
+	// MARK: Properties For Download Test
+	var downloadTask : URLSessionDownloadTask!
+	var backgroundSession: URLSession!
+	var requestDownload = false
+	
 
-class InterfaceController: WKInterfaceController {
+	let sampleURL = URL(string: "https://www.drewwestcott.co.uk/wp-content/themes/lab-responsive-bootstrap-child/images/curtains1.png")!
 
-    override func awake(withContext context: Any?) {
+	// MARK: Properties For Download Test
+	var player: WKAudioFilePlayer!
+	@IBOutlet weak var audioLabel: WKInterfaceLabel!
+	@IBOutlet var podcastButton: WKInterfaceButton!
+	
+	override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        // Configure interface objects here.
-    }
+		WKExtension.shared().delegate = self
+		podcastButton.setTitle("99 Percent Invisible")
+		if requestDownload == false {
+			downloadPodcast()
+			requestDownload = true
+		}
+	}
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+		
+		scheduleBackgroundRefresh()
     }
-    
+	
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+		audioLabel.setText("...")
     }
+
+	func downloadPodcast() {
+		let backgroundConfigObject = URLSessionConfiguration.background(withIdentifier: "uk.co.drewwestcott.cotvalley")
+		let cachesDirectoryURL = try! FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+		//let cacheURL = cacheDirectoryURL.append
+		backgroundConfigObject.sessionSendsLaunchEvents = true
+		let backgroundSession = URLSession(configuration: backgroundConfigObject)
+		
+		let downloadTask = backgroundSession.downloadTask(with: sampleURL)
+		downloadTask.resume()
+	}
+	
+	@IBAction func playFile() {
+		playPodcast()
+
+	}
+
+	
+	func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+		print("File Downloaded")
+//		self.urlSessionTask.setTaskCompleted()
+
+	}
+	
+	func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+		print("Data written \(totalBytesWritten) bytes")
+	}
+	// MARK: Podcast Player functions
+
+	func playPodcast() {
+		
+		let filePath = Bundle.main.path(forResource: "246-Usonia", ofType: "mp3")!
+		let fileURL = NSURL.fileURL(withPath: filePath)
+		let asset = WKAudioFileAsset(url: fileURL)
+		let playerItem = WKAudioFilePlayerItem(asset: asset)
+		
+		let player = WKAudioFilePlayer(playerItem: playerItem)
+			audioLabel.setText("Playing")
+			player.play()
+	}
+	
+	func scheduleBackgroundRefresh() {
+		
+		let fireDate = Date(timeIntervalSinceNow: 120)
+		// optional, any SecureCoding compliant data can be passed here
+		let userInfo = ["reason" : "background update"] as NSDictionary
+		
+		WKExtension.shared().scheduleBackgroundRefresh(withPreferredDate: fireDate, userInfo: userInfo) { (error) in
+			if (error == nil) {
+				print("successfully scheduled background task, use the crown to send the app to the background and wait for handle:BackgroundTasks to fire.")
+			}
+		}
+
+	}
+	
+	func handle(_ backgroundTasks: Set<WKRefreshBackgroundTask>) {
+		// Sent when the system needs to launch the application in the background to process tasks. Tasks arrive in a set, so loop through and process each one.
+		for task in backgroundTasks {
+			// Use a switch statement to check the task type
+			
+			if let urlTask = task as? WKURLSessionRefreshBackgroundTask {
+			let backgroundConfigObject = URLSessionConfiguration.backgroundSessionConfiguration(urlTask.sessionIdentifier)
+			let backgroundSession = URLSession(configuration: backgroundConfigObject,
+			                                   delegate: self,
+			                                   delegateQueue: nil)
+			// receive data via URLSessionDownloadDelegate
+			pendingBackgroundTasks.append(task)
+		} else {
+			task.setTaskCompleted()   // make sure to complete all tasks
+		}
+		
+			switch task {
+			case let backgroundTask as WKApplicationRefreshBackgroundTask:
+				// Be sure to complete the background task once you’re done.
+				
+				if (WKExtension.shared().applicationState == .background) {
+					if task is WKApplicationRefreshBackgroundTask {
+						print("application task received, starting URL session")
+//						downloadPodcast()
+					}
+				}
+				
+				//backgroundTask.setTaskCompleted()
+			case let snapshotTask as WKSnapshotRefreshBackgroundTask:
+				// Snapshot tasks have a unique completion call, make sure to set your expiration date
+				snapshotTask.setTaskCompleted(restoredDefaultState: true, estimatedSnapshotExpiration: Date.distantFuture, userInfo: nil)
+			case let connectivityTask as WKWatchConnectivityRefreshBackgroundTask:
+				// Be sure to complete the connectivity task once you’re done.
+				connectivityTask.setTaskCompleted()
+			case let urlSessionTask as WKURLSessionRefreshBackgroundTask:
+				// Be sure to complete the URL session task once you’re done.
+					let backgroundConfigObject = URLSessionConfiguration.background(withIdentifier: urlSessionTask.sessionIdentifier)
+					let backgroundSession = URLSession(configuration: backgroundConfigObject, delegate: self, delegateQueue: nil)
+				print("Session continuing")
+			default:
+				// make sure to complete unhandled task types
+				task.setTaskCompleted()
+			}
+		}
+	}
 
 }
--- downloadFile/ViewController.swift
+++ downloadFile/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, URLSessionDownloadDelegate {
+	
+	var downloadTask: URLSessionDownloadTask!
+	var backgroundSession: URLSession!
+	
+	var audioPlayer = AVAudioPlayer()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+
+		let backgroundSessionConfiguration = URLSessionConfiguration.background(withIdentifier: "backgroundSession")
+		backgroundSession = URLSession(configuration: backgroundSessionConfiguration, delegate: self, delegateQueue: OperationQueue.main)
+		
+		let url = URL(string: "https://www.drewwestcott.co.uk/Sound/01%20Cot%20Valley.m4a")!
+		downloadTask = backgroundSession.downloadTask(with: url)
+		downloadTask.resume()
+		
+		
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -20,6 +34,48 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+		print("Downloaded")
+		let path = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+		let documentDirectoryPath:String = path[0]
+		let fileManager = FileManager()
+		let destinationURLForFile = URL(fileURLWithPath: documentDirectoryPath.appendingFormat("/file.m4a"))
+		print(destinationURLForFile)
+		
+		if fileManager.fileExists(atPath: destinationURLForFile.path){
+			playFileWithPath(path: destinationURLForFile.path)
+		}
+		else{
+			do {
+				try fileManager.moveItem(at: location, to: destinationURLForFile)
+				// show file
+				playFileWithPath(path: destinationURLForFile.path)
+			}catch{
+				print("An error occurred while moving file to destination url")
+			}
+		}
+
+	}
+	
+	func playFileWithPath(path: String){
+		let isFileFound:Bool? = FileManager.default.fileExists(atPath: path)
+		if isFileFound == true{
+			let podcast = URL(fileURLWithPath: path)
+			
+			//var podcast = NSURL(fileURLWithPath: Bundle.main.path(forResource: "file", ofType: "m4a")!)
+
+			var error : NSError?
+			do {
+				audioPlayer = try AVAudioPlayer(contentsOf:  podcast as URL)
+				audioPlayer.play()
+			} catch {
+				print("Error '(error)")
+			}
+			
+//			viewer.delegate = self
+//			viewer.presentPreview(animated: true)
+		}
+	}
 
 }
 
